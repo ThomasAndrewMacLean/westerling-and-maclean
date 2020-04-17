0c6259464f0f0e74cf5f28f228ef36e0a82c5dbf
ThomasAndrewMacLean <hello@thomasmaclean.be>
2020-04-17 22:37:35 +0200

    cursor hack

--- components/Plant.js
+++ components/Plant.js
@@ -24,6 +24,8 @@ const Section = styled.div`
   /* background-blend-mode: luminosity; */
   /* filter: sepia(1); */
   height: 100vh;
+  cursor: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg'  width='40' height='48' viewport='0 0 100 100' style='fill:black;font-size:24px;'><text y='50%'>☘️</text></svg>"),
+    auto;
 `;
 
 const Left = styled.div`
