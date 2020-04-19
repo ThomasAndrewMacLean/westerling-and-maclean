f42259b7d0e00b86d9f3c0058d92c0dd771c9460
ThomasAndrewMacLean <hello@thomasmaclean.be>
2020-04-19 18:23:49 +0200

    pwa it up 🛥

--- pages/_document.js
+++ pages/_document.js
@@ -24,6 +24,79 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
+          <link rel="manifest" href="/manifest.json" />
+
+          <meta name="application-name" content="Westerling &amp; MacLean" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="default"
+          />
+          <meta
+            name="apple-mobile-web-app-title"
+            content="Westerling &amp; MacLean"
+          />
+          <meta name="description" content="Westerling &amp; MacLean" />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          {/* <meta
+            name="msapplication-config"
+            content="/browserconfig.xml"
+          /> */}
+          <meta name="msapplication-TileColor" content="#2B5797" />
+          <meta name="msapplication-tap-highlight" content="no" />
+          <meta name="theme-color" content="#000000" />
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+          />
+
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          {/* <link
+            rel="mask-icon"
+            href="/safari-pinned-tab.svg"
+            color="#5bbad5"
+          /> */}
+          <link rel="shortcut icon" href="/favicon.ico" />
+          {/* <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
+          /> */}
+
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:url" content="https://yourdomain.com" />
+          <meta name="twitter:title" content="Westerling &amp; MacLean" />
+          <meta name="twitter:description" content="Westerling &amp; MacLean" />
+          {/* <meta
+            name="twitter:image"
+            content="https://yourdomain.com/android-chrome-192x192.png"
+          />
+          <meta name="twitter:creator" content="@DavidWShadow" /> */}
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="Westerling &amp; MacLean" />
+          <meta property="og:description" content="Westerling &amp; MacLean" />
+          <meta property="og:site_name" content="Westerling &amp; MacLean" />
+          {/* <meta property="og:url" content="https://yourdomain.com" />
+          <meta
+            property="og:image"
+            content="https://yourdomain.com/apple-touch-icon.png"
+          /> */}
           {/* Step 5: Output the styles in the head  */}
           {this.props.styleTags}
         </Head>
